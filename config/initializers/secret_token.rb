1ade0177dc36616791dbb411c364886a7d3fb129
Nick Steele <nickelsteele@gmail.com>
2012-09-04 14:50:16 -0400

    This is the working build of the PragProg rails example
    Pages 1 through 125 implemented

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookstore::Application.config.secret_token = '69f0eeb767ae93c26b1c9691531b9c9c1a03d506c8df2dab425401cdc71d757ae264e71b31bc29fd2845490753c2e1b6c949621ce3ade1b0f7fe32935dbe3cd5'
---
+++ test/unit/product_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+  test "product is not valid without a unique title" do
+    product = Product.new(:title       => products(:ruby).title,
+                          :description => "buttsandstruts",
+                          :price       => 19.95,
+                          :image_url   => "fred.gif")
+    assert !product.save
+    assert "has already been taken", product.errors[:title].join('; ')
+  end
+
+  test "product price must not be negative" do
+     product = Product.new(:title          => "My Book Title",
+                           :description    => "A book bout titles",
+                           :image_url      => "title.jpg")
+     product.price = -1
+     assert product.invalid?
+     assert_equal "must be greater than or equal to 0.01",
+       product.errors[:price].join('; ')
+
+     product.price = 0
+     assert product.invalid?
+     assert_equal "must be greater than or equal to 0.01",
+       product.errors[:price].join('; ')
+
+     product.price = 1
+     assert product.valid?
+   end
+
+   def new_product(image_url)
+                 Product.new(:title          => "My Book Title",
+                             :description    => "What do you want?",
+                             :price          => 1,
+                             :image_url      => image_url)
+   end
+
+   test "image_url" do
+     ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+     bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+     ok.each do |name|
+       assert new_product(name).valid?, "#{name} shouldn't be invalid"
+     end
+
+     bad.each do |name|
+       assert new_product(name).invalid?, "#{name} shouldn't be valid"
+     end
+   end
+end
